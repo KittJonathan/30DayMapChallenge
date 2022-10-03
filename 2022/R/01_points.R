bb80ceff1ae3196cc00fd14521713825beba9dba
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-10-03 10:20:20 +0200

    import data

--- 2022/R/01_points.R
+++ 2022/R/01_points.R
@@ -2,3 +2,20 @@
 # 2022
 # Day 1 : Points
 # Last updated : 2022-10-03
+
+# Load packages ----
+
+library(tidyverse)
+
+# Extract data ----
+
+url <- "https://fr.wikipedia.org/wiki/Point_chaud_(géologie)"
+
+webpage <- rvest::read_html(url)
+
+tables <- rvest::html_nodes(webpage, "table.wikitable") %>%
+  rvest::html_table(header = TRUE, na.strings = c(NA, ""), convert = TRUE)
+
+hotspots <- tables[[1]]
+
+rm(tables, url, webpage)
