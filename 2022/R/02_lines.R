3bfc82778f2be53260e0f2f26b9c339d17964f37
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-11-02 08:43:28 +0100

    updated day 2 map

--- 2022/R/02_lines.R
+++ 2022/R/02_lines.R
@@ -1,28 +1,24 @@
 # 30DayMapChallenge
 # 2022
 # Day 2 : Lines
-# Last updated : 2022-10-11
+# Last updated : 2022-11-02
 
 # Load packages ----
 
-# library(osmdata)
 library(reshape2)
 library(showtext)
 library(tidyverse)
-# library(geojsonio)
-# library(jsonlite)
-# library(sp)
-# library(broom)
 
 # Import fonts ----
 
 font_add_google("Zen Tokyo Zoo", "Zen Tokyo Zoo")
-# font_add_google("Glory", "Glory")
 showtext_auto()
 
-# Volcano dataset ----
+# Clean data ----
 
 d1 <- reshape2::melt(volcano)
+
+# Create map ----
 
 (p <- ggplot() +
   geom_contour(data = d1, aes(Var1, Var2, z = value,
@@ -43,87 +39,6 @@ d1 <- reshape2::melt(volcano)
                                     margin = margin(b = 10)))
 )
 
+# Export map ----
+
 ggsave("2022/maps/02_lines.png", p, dpi = 320, height = 6, width = 12)
-
-# Get data ----
-
-getbb("France")
-
-d1 <- getbb("Lyon") |> 
-  opq() |> 
-  add_osm_feature(key = "route",
-                  value = "train") |> 
-  osmdata_sf()
-
-ggplot() +
-  geom_sf(data = d1$osm_lines)
-
-# Extract data ----
-
-# https://github.com/fraxen/tectonicplates
-# https://r-graph-gallery.com/325-background-map-from-geojson-format-in-r.html
-
-# d1 <- geojsonio::geojson_read("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json", what = "sp")
-plates <- geojsonio::geojson_read("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json", what = "sp")
-
-# tidy_plates <- tidy(plates)
-
-# africa <- tidy(plates[plates@data$PlateName == "Africa", ])
-
-# test <- d1[d1@data$Name == "CA-NA", ]
-
-# subduction <- d1[d1@data$Type == "subduction", ]
-# sub_tidy <- tidy(subduction)
-
-# d2 <- tidy(d1)
-
-# tect <- d1[d1@data$Type != "subduction", ]
-# tect_tidy <- tidy(tect)
-
-# bird <- d1[d1@data$Source == "Peter Bird, June 2002", ]
-# bird_tidy <- tidy(bird)
-
-# eu_af <- tidy(d1[d1@data$Name %in% c("EU-AF", "EU\\AF", "EU/AF", "NA-AF"), ])
-
-world <- map_data("world")
-
-plates_list <- sort(unique(plates@data$PlateName))
-
-plates_tidy <- tidy(plates)
-
-ggplot() +
-  geom_polygon(data = world,
-               aes(x = long, y = lat, group = group),
-               colour = "#1b1d46", fill = "#1b1d46") +
-  geom_polygon(data = tidy(plates[plates@data$PlateName == plates_list[8], ]),
-               aes(x = long, y = lat, group = group),
-               colour = "red", fill = NA, size = 2)
-
-ggplot() +
-  geom_polygon(data = world,
-               aes(x = long, y = lat, group = group),
-               colour = "#1b1d46", fill = "#1b1d46") +
-  geom_polygon(data = tidy(plates[plates@data$PlateName ==  "Antarctica", ]),
-               aes(x = long, y = lat, group = group),
-               colour = "red", fill = NA, size = 1, alpha = 0.5) +
-  coord_map("ortho", orientation = c(-90, 0, 0))
-
-
-  geom_polygon(data = tidy(plates[plates@data$PlateName %in% c("Africa", "Antarctica", "Nazca",
-                                                               "South America"), ]),
-               aes(x = long, y = lat, group = group),
-               colour = "red", fill = "red", size = 1, alpha = 0.5)
-
-ggplot() +
-  geom_polygon(data = world,
-               aes(x = long, y = lat, group = group),
-               colour = "#1b1d46", fill = "#1b1d46") +
-  geom_polygon(data = tidy_plates, aes( x = long, y = lat, group = group), color = "red", size = 1, fill = NA) 
-
-
-ggplot() +
-  geom_line(data = tect_tidy, aes( x = long, y = lat, group = group), color = "white", fill = NA) +
-  # theme_void() +
-  coord_map()
-
-
